092f05a20723525c2833792c879bab82a25e25cd
momunovN <152641534+momunovN@users.noreply.github.com>
2024-10-09 11:34:14 +0300

    0910-24

---
+++ momunovv0710/app/src/main/java/com/example/momunovv0710/AboutActivity.java
@@ -0,0 +1,31 @@
+package com.example.momunovv0710;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        Button btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutActivity.this, MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(AboutActivity.this, "Возврат на главный экран", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ momunovv0710/app/src/main/java/com/example/momunovv0710/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.momunovv0710;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnAbout = findViewById(R.id.btn_about);
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Переход на экран \"О приложении\"", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btnContacts = findViewById(R.id.btn_contacts);
+        btnContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ContactsActivity.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Переход на экран \"Контакты\"", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
